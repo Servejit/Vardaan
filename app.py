01e6b05308aa35a63879a494834ec493a5c62cb7
Servejit <servejitsingh@gmail.com>
2026-09-14 15:56:15 +0530

    Add SUPABASE_SERVICE_KEY to Supabase client

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ def get_supabase():
     return create_client(
         st.secrets["SUPABASE_URL"],
         st.secrets["SUPABASE_KEY"]
+        SUPABASE_SERVICE_KEY = st.secrets["SUPABASE_SERVICE_KEY"]
     )
 
 
